5092c90d7722b6dd2baaa1efb77891ebbe3687d5
Daniel Remijes <danielremijes2002@gmail.com>
2024-04-11 12:42:01 +0530

    added title and description to aticles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_11_065827) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_11_070832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
   end
 
 end
