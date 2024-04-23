3d0d5ddbd106f82e778633b1488671b91134d186
Daniel Remijes <danielremijes2002@gmail.com>
2024-04-23 16:22:59 +0530

    added an edit action to the application

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,4 +19,17 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
