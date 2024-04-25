ebdf32baaeda1238b67ed4c57228d72c8ccd2278
Daniel Remijes <danielremijes2002@gmail.com>
2024-04-25 11:55:08 +0530

    added an initialized devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "/articles/first", to: "articles#first_article"
