799d421ed4d9fb863173fa52e2cecda9531b9861
Daniel Remijes <danielremijes2002@gmail.com>
2024-05-13 10:35:47 +0530

    added a navbar to list all the links

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "articles#index"
 end
