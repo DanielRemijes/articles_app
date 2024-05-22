1819b6b0eb31aea58f24a83b4c372c5b8283c962
Daniel Remijes <danielremijes2002@gmail.com>
2024-05-22 11:51:16 +0530

    added the swiper js to show the pictures in slideshow

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,13 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("turbo:load", () => {
+  var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
+});
