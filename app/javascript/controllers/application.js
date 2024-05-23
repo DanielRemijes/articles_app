adbacb040df2efe20f8dd0b164bb74ed552196ad
Daniel Remijes <danielremijes2002@gmail.com>
2024-05-23 12:29:44 +0530

    added the slider function in show page itself

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,13 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-document.addEventListener("turbo:load", () => {
-  var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    loop: true,
-  });
-});
